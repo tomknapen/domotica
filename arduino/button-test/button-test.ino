f93fb8ba05d46c2762925548a093e13623cc01c7
Tom <tom.knapen@kbc.be>
2020-01-21 22:31:44 +0100

    easybutton

--- arduino/button-test/button-test.ino
+++ arduino/button-test/button-test.ino
@@ -1,22 +1,27 @@
-#define NUMBER_OF_PINS 10
-const int buttonPins[NUMBER_OF_PINS] = {22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+#include <EasyButton.h>
+
+#define NUMBER_OF_BUTTONS 10
+
+EasyButton buttons[NUMBER_OF_BUTTONS] = {
+  (22), (23), (24), (25), (26),
+  (27), (28), (29), (30), (31),
+};
+
+void onButtonPressed() {
+  Serial.println("Button pressed!");
+}
 
 void setup() {
   Serial.begin(115200);
-  
-  for (int i = 0; i < NUMBER_OF_PINS; i++) {
-    pinMode(buttonPins[i], INPUT_PULLUP);
-  }
 
+  for (int i = 0; i < NUMBER_OF_BUTTONS; i++){
+    buttons[i].begin();
+    buttons[i].onPressed(onButtonPressed);
+  }
 }
 
 void loop() {
-  for (int i = 0; i < NUMBER_OF_PINS; i++) {
-    if (digitalRead(buttonPins[i]) == HIGH) {
-      continue;
-    }
-
-    Serial.print("Input detected on pin ");
-    Serial.println(buttonPins[i]);
+  for (int i = 0; i < NUMBER_OF_BUTTONS; i++){
+    buttons[i].read();
   }
 }
