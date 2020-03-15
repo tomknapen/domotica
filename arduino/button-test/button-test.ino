1c947d18c5025b8ab5243811de2ec0024eab783d
Tom <tom.knapen@kbc.be>
2020-03-15 16:25:56 +0100

    pin array

--- arduino/button-test/button-test.ino
+++ arduino/button-test/button-test.ino
@@ -2,6 +2,11 @@
 
 #define NUMBER_OF_BUTTONS 10
 
+int buttonPins[NUMBER_OF_BUTTONS] = {
+  22, 23, 24, 25, 26,
+  27, 28, 29, 30, 31,
+};
+
 PushButton buttons[NUMBER_OF_BUTTONS] = {
   (22), (23), (24), (25), (26),
   (27), (28), (29), (30), (31),
@@ -11,16 +16,9 @@ PushButton buttons[NUMBER_OF_BUTTONS] = {
 void setup() {
   Serial.begin(115200);
 
-  pinMode(22, INPUT_PULLUP);
-  pinMode(23, INPUT_PULLUP);
-  pinMode(24, INPUT_PULLUP);
-  pinMode(25, INPUT_PULLUP);
-  pinMode(26, INPUT_PULLUP);
-  pinMode(27, INPUT_PULLUP);
-  pinMode(28, INPUT_PULLUP);
-  pinMode(29, INPUT_PULLUP);
-  pinMode(30, INPUT_PULLUP);
-  pinMode(31, INPUT_PULLUP);
+  for (int i = 0; i < NUMBER_OF_BUTTONS; i++){
+    pinMode(buttonPins[i], INPUT_PULLUP);
+  }
   
   for (int i = 0; i < NUMBER_OF_BUTTONS; i++){
     PushButton* button = &buttons[i];
@@ -32,21 +30,26 @@ void setup() {
 
 void loop() {
   for (int i = 0; i < NUMBER_OF_BUTTONS; i++){
+    int pin = buttonPins[i];
     PushButton* button = &buttons[i];
 
     button->update();
 
     if (button->isClicked()){
-      Serial.println("clicked");
+      Serial.print("clicked ");
+      Serial.println(pin);
     }
         if (button->isDoubleClicked()){
-      Serial.println("double clicked");
+      Serial.print("double clicked ");
+      Serial.println(pin);
     }
         if (button->isHeld()){
-      Serial.println("held");
+      Serial.print("held ");
+      Serial.println(pin);
     }
-    if (button->isActive()){
-//      Serial.println("active");
-    }
+//    if (button->isActive()){
+//      Serial.println(");
+//    }
+
   }
 }
