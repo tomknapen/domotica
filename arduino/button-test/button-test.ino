655ab25cb9dcb6825d44ed1a7fb76854f11100a4
Tom <tom.knapen@kbc.be>
2020-03-15 17:45:16 +0100

    dynamic array size instead of predefined constant

--- arduino/button-test/button-test.ino
+++ arduino/button-test/button-test.ino
@@ -1,12 +1,12 @@
 // using library https://github.com/avandalen/avdweb_Switch
 #include <avdweb_Switch.h>
 
-#define NUMBER_OF_BUTTONS 10
-
-int buttonPins[NUMBER_OF_BUTTONS] = {
+int buttonPins[] = {
   22, 23, 24, 25, 26,
   27, 28, 29, 30, 31,
 };
+
+#define NUMBER_OF_BUTTONS sizeof(buttonPins)/sizeof(int)
 
 Switch *buttons[NUMBER_OF_BUTTONS];
 
@@ -19,11 +19,22 @@ void setup() {
 }
 
 void loop() {
+  int startTime = millis();
+  
   for (int i = 0; i < NUMBER_OF_BUTTONS; i++){
     int pin = buttonPins[i];
     Switch* button = buttons[i];
 
     processButton(button, pin);
+  }
+  
+  int endTime = millis();
+
+  int duration = endTime - startTime;
+
+  if (duration > 2){
+    Serial.print("elapsed: ");
+    Serial.println(duration);
   }
 }
 
