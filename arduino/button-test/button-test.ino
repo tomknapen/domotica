851b31c4c9a91c3480198e4789a3b84c0c8cce7d
Tom <tom.knapen@kbc.be>
2020-03-15 17:57:12 +0100

    all button pins

--- arduino/button-test/button-test.ino
+++ arduino/button-test/button-test.ino
@@ -2,8 +2,10 @@
 #include <avdweb_Switch.h>
 
 int buttonPins[] = {
-  22, 23, 24, 25, 26,
-  27, 28, 29, 30, 31,
+  A0, 1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, 
+  2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 
+  22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 
+  42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54,
 };
 
 #define NUMBER_OF_BUTTONS sizeof(buttonPins)/sizeof(int)
@@ -32,7 +34,7 @@ void loop() {
 
   int duration = endTime - startTime;
 
-  if (duration > 2){
+  if (duration > 3){
     Serial.print("elapsed: ");
     Serial.println(duration);
   }
