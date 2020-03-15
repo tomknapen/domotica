fe595ef6378f414cfd520a096aa2f5510578a74b
Tom <tom.knapen@kbc.be>
2020-03-15 17:00:47 +0100

    cleanup

--- arduino/button-test/button-test.ino
+++ arduino/button-test/button-test.ino
@@ -1,4 +1,5 @@
-#include <PushButton.h>
+// using library https://github.com/avandalen/avdweb_Switch
+#include <avdweb_Switch.h>
 
 #define NUMBER_OF_BUTTONS 10
 
@@ -7,49 +8,39 @@ int buttonPins[NUMBER_OF_BUTTONS] = {
   27, 28, 29, 30, 31,
 };
 
-PushButton buttons[NUMBER_OF_BUTTONS] = {
+Switch buttons[NUMBER_OF_BUTTONS] = {
   (22), (23), (24), (25), (26),
   (27), (28), (29), (30), (31),
 };
 
-
 void setup() {
   Serial.begin(115200);
-
-  for (int i = 0; i < NUMBER_OF_BUTTONS; i++){
-    pinMode(buttonPins[i], INPUT_PULLUP);
-  }
-  
-  for (int i = 0; i < NUMBER_OF_BUTTONS; i++){
-    PushButton* button = &buttons[i];
-
-    button->setActiveLogic(LOW);
-    button->setHoldTime(1000);
-  }
 }
 
 void loop() {
   for (int i = 0; i < NUMBER_OF_BUTTONS; i++){
     int pin = buttonPins[i];
-    PushButton* button = &buttons[i];
+    Switch* button = &buttons[i];
 
-    button->update();
+    processButton(button, pin);
+  }
+}
 
-    if (button->isClicked()){
-      Serial.print("clicked ");
-      Serial.println(pin);
-    }
-        if (button->isDoubleClicked()){
-      Serial.print("double clicked ");
-      Serial.println(pin);
-    }
-        if (button->isHeld()){
-      Serial.print("held ");
-      Serial.println(pin);
-    }
-//    if (button->isActive()){
-//      Serial.println(");
-//    }
+void processButton(Switch* button, int pin) {
+  button->poll();
 
+  if (button->singleClick()){
+    Serial.print("clicked ");
+    Serial.println(pin);
+  }
+  
+  if (button->doubleClick()){
+    Serial.print("double clicked ");
+    Serial.println(pin);
+  }
+      
+  if (button->longPress()){
+    Serial.print("held ");
+    Serial.println(pin);
   }
 }
