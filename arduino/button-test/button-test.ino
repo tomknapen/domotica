1b99c7b51c9538752131440e5abd1a84d94275e5
Tom <tom.knapen@kbc.be>
2020-03-15 16:13:33 +0100

    buttons

--- arduino/button-test/button-test.ino
+++ arduino/button-test/button-test.ino
@@ -1,31 +1,44 @@
-#include "DomoButton.h"
+#include <PushButton.h>
 
 #define NUMBER_OF_BUTTONS 10
 
-DomoButton buttons[NUMBER_OF_BUTTONS] = {
-  (22), (23), (24), (25), (26),
-  (27), (28), (29), (30), (31),
-};
+//PushButton buttons[NUMBER_OF_BUTTONS] = {
+//  (22), (23), (24), (25), (26),
+//  (27), (28), (29), (30), (31),
+//};
 
-void onButtonPressed() {
-  Serial.println("Button pressed!");
-}
+PushButton button(22);
+
 
 void setup() {
   Serial.begin(115200);
 
-  for (int i = 0; i < NUMBER_OF_BUTTONS; i++){
-    buttons[i].begin();
-    buttons[i].onPressed(onButtonPressed);
-  }
+  pinMode(22, INPUT_PULLUP);
+//  for (int i = 0; i < NUMBER_OF_BUTTONS; i++){
+//    PushButton button = buttons[22];
+//
+    button.setActiveLogic(LOW);
+    button.setHoldTime(1000);
+//  }
 }
 
 void loop() {
-  for (int i = 0; i < NUMBER_OF_BUTTONS; i++){
-    buttons[i].read();
+//  for (int i = 0; i < NUMBER_OF_BUTTONS; i++){
+//    PushButton button = buttons[];
+//
+    button.update();
 
-    if (buttons[i].isPressed()){
-      Serial.println("still pressed");
+    if (button.isClicked()){
+      Serial.println("clicked");
     }
-  }
+        if (button.isDoubleClicked()){
+      Serial.println("double clicked");
+    }
+        if (button.isHeld()){
+      Serial.println("held");
+    }
+    if (button.isActive()){
+      Serial.println("active");
+    }
+//  }
 }
